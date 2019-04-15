8bbf12aa0c1865a6b75589b6f279a7491e8cb07b
Penny <31910522+Pennyzct@users.noreply.github.com>
2019-04-15 15:41:20 +0800

    Add files via upload

---
+++ report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# module vhost_vsock is removed when no one is using it
+while :
+do
+        if [ "$(lsmod | awk '{ if($1 == "vhost_vsock") print $3;}')" == "0" ]; then
+                modprobe -r vhost_vsock
+                break
+        fi
+done
+
+echo "./grabdata.sh -t"
+./grabdata.sh -t
+mkdir ../results/run2
+mv ../results/*.json ../results/run2/
